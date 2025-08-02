40232440d8e5f3553c97eb7f38b87d06d39b8d81
josevinoth <101917197+josevinoth@users.noreply.github.com>
2025-08-02 09:38:21 +0530

    Jose (02-Aug-2025) mobile view changes

--- epe/epe_app/sub_models/parameter_definition_mod.py
+++ epe/epe_app/sub_models/parameter_definition_mod.py
@@ -4,7 +4,7 @@ from ..models import datatype_Info,owner_info,digital_source_info,dictionary_Inf
 class prameter_definition_info(models.Model):
     pd_id = models.CharField(max_length=100, blank=True,null=True, default='')
     pd_name = models.CharField(max_length=100, null=True, default='')
-    pd_digital_source=models.ForeignKey(digital_source_info,on_delete=models.CASCADE,default=1)
+    pd_digital_source = models.ManyToManyField(digital_source_info)
     pd_owner=models.ForeignKey(owner_info,on_delete=models.CASCADE,default=1)
     pd_unit_type=models.ForeignKey(unit_type_info,on_delete=models.CASCADE,default=1)
     pd_library =models.ForeignKey(dictionary_Info,on_delete=models.CASCADE,default=1)
