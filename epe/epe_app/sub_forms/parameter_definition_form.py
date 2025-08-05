adb7c044fb187e32e2b194d606ef35ffe75b1bfd
josevinoth <101917197+josevinoth@users.noreply.github.com>
2025-08-05 15:35:45 +0530

    Jose (05-Aug-2025) mobile view changes

--- epe/epe_app/sub_forms/parameter_definition_form.py
+++ epe/epe_app/sub_forms/parameter_definition_form.py
@@ -2,11 +2,6 @@ from django import forms
 from ..models import prameter_definition_info,digital_source_info
 
 class parameter_definition_form(forms.ModelForm):
-    pd_digital_source = forms.ModelMultipleChoiceField(
-        queryset=digital_source_info.objects.all(),
-        widget=forms.SelectMultiple,
-        required=False
-    )
     class Meta:
         model = prameter_definition_info
         fields = '__all__'
