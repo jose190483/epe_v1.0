52a328e5369b3dd0c457bee9b35f7815888f7664
waltjos01 <josevinoth.walter@bakerhughes.com>
2025-08-06 12:52:52 +0530

    UI changes (06-Aug-2025)

--- epe/epe_app/sub_models/project_mod.py
+++ epe/epe_app/sub_models/project_mod.py
@@ -1,6 +1,6 @@
 from django.db import models
 class project_info(models.Model):
-    p_project_id= models.CharField(max_length=100, null=True,default='')
+    p_project_id= models.CharField(max_length=100, null=True,blank=True)
     p_project_name = models.CharField(max_length=100, null=True,default='')
     p_customer_name = models.CharField(max_length=100, null=True,default='')
     p_location= models.CharField(max_length=100, null=True,default='')
